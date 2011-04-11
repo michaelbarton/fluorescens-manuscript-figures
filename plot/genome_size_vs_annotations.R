210a28d1f3a85c499915e2cb5cf08492c8488ddb
Michael Barton <mail@michaelbarton.me.uk>
2011-04-11 16:08:45 -0400

    Highlight R124 in genome plot

--- plot/genome_size_vs_annotations.R
+++ plot/genome_size_vs_annotations.R
@@ -24,18 +24,20 @@ sizes <- adply(files,c(1),.parallel=T,function(x){
 
   data.frame(
    genes  = length(read.fasta(file = genes, as.string = TRUE, seqtype = "DNA")),
-   genome = as.integer(strsplit(readLines(file(genome,"r"), 1),'\\s+')[[1]][3])
+   genome = as.integer(strsplit(readLines(file(genome,"r"), 1),'\\s+')[[1]][3]),
+   Genome = "Pseudomonas"
   )
 })
 
-sizes <- rbind(sizes,data.frame(X1 = 0, genes = genes, genome=assembly))[,2:3]
+sizes <- rbind(sizes,
+  data.frame(X1 = 0, genes = genes, genome=assembly,Genome="R124"))[,2:4]
 
-p <- ggplot(sizes,aes(genes/1000,genome/1000000))
-p <- p + geom_point()
-p <- p + scale_x_continuous("Number of genes (000s)")
-p <- p + scale_y_continuous("Genome size (MBp)")
+p <- ggplot(sizes,aes(y=genes/1000,x=genome/1000000,colour=Genome))
+p <- p + geom_point(size=10)
+p <- p + scale_y_continuous("Gene Count (000s)")
+p <- p + scale_x_continuous("Size (MBp)")
 p <- p + theme_bw()
 
-png('out/genome_size_vs_annotations.png')
+png('out/genome_size_vs_annotations.png',width=600)
 print(p)
 graphics.off()
