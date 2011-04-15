2c2eeaf745640c4153a976c634ae9ee2272cdffe
Michael Barton <mail@michaelbarton.me.uk>
2011-04-15 15:05:20 -0400

    Plot alignments along x-axis of chart

--- plot/genome_dot_plot.R
+++ plot/genome_dot_plot.R
@@ -27,14 +27,41 @@ data$reference <- as.factor(find.replace(data$reference,
   c("Pf-01","Pf-5","SBW-25")
   ))
 
-png("out/figure_2.png",width = 600)
 
-p <- ggplot(data,aes(x = V1/1000000, xend = V2/1000000,
-  y = V3/1000000, yend = V4/1000000,color=reference))
-p <- p + geom_segment(size = 4)
-p <- p + scale_x_continuous("Reference Genome Position (MBp)")
-p <- p + scale_y_continuous("R124 Genome Position (MBp)")
-p <- p + theme_bw()
+base <- ggplot(data,aes(color=reference)) + theme_bw()
 
-print(p)
+p1 <- base + geom_segment(size = 6,aes(x = V1/1000000, xend = V2/1000000,
+  y = V3/1000000, yend = V4/1000000))
+p1 <- p1 + scale_y_continuous("R124 Genome Position (MBp)")
+p1 <- p1 + xlab(NULL)
+
+p2 <- base + geom_density(size = 1, adjust = 1/3, aes(x = (V2 + V1)/2000000))
+p2 <- p2 + scale_x_continuous("Reference Genome Position (MBp)")
+p2 <- p2 + scale_y_continuous("Density",breaks = c(0))
+
+legend <- p2 + opts(keep = "legend_box")
+p1 <- p1 + opts(legend.position = "none")
+p2 <- p2 + opts(legend.position = "none")
+
+Layout <- grid.layout(
+  nrow    = 2,
+  ncol    = 2,
+  widths  = unit(c(2, 0.4), c("null", "null")),
+  heights = unit(c(5, 2),   c("null", "null"))
+)
+
+vplayout <- function(...) {
+  grid.newpage()
+  pushViewport(viewport(layout = Layout))
+}
+
+subplot <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
+
+png("out/figure_2.png", width = 600, height = 600)
+
+vplayout()
+print(p1,     vp = subplot(1, 1))
+print(p2,     vp = subplot(2, 1))
+print(legend, vp = subplot(1:2, 2))
+
 graphics.off()
