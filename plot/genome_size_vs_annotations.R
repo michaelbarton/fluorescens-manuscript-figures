15671f4b59b8b1f64972dce3058e447527e7f376
Michael Barton <mail@michaelbarton.me.uk>
2011-07-18 15:47:40 -0400

    Replot genome size vs gene count

--- plot/genome_size_vs_annotations.R
+++ plot/genome_size_vs_annotations.R
@@ -1,39 +1,16 @@
 #!/usr/bin/env Rscript
 
-library(seqinr)
 library(plyr)
 library(ggplot2)
 
-annotations <- read.csv('data/genome/annotation/gene_list.csv')
 
-plasmid <- grep('scaffold00008',annotations$Scaffold.Name)
-plasmid <- plasmid
 
-genome <- union(grep('scaffold',annotations$Scaffold.Name),
-  grep('contig00001',annotations$Scaffold.Name))
-genome <- setdiff(genome, plasmid)
+sizes <- read.csv('data/genome_size.csv')
+sizes <- subset(sizes, source == "genome")
 
-genes    <- length(genome)
-assembly <- length(read.fasta(file = 'data/genome/assembly/assembly.fna')[[1]])
-
-files <- dir('data/reference/gene',pattern='*',recursive=TRUE)
-files <- files[grep('genome',files)]
-sizes <- adply(files,c(1),.parallel=T,function(x){
-  genes  <- paste('data/reference/gene',x,sep="/")
-  genome <- paste('data/reference/genomes/',(strsplit(x,'\\.')[[1]][1]),".gb",sep="")
-
-  data.frame(
-   genes  = length(read.fasta(file = genes, as.string = TRUE, seqtype = "DNA")),
-   genome = as.integer(strsplit(readLines(file(genome,"r"), 1),'\\s+')[[1]][3]),
-   Genome = "Pseudomonas"
-  )
-})
-
-sizes <- rbind(sizes,
-  data.frame(X1 = 0, genes = genes, genome=assembly,Genome="R124"))[,2:4]
-
-p <- ggplot(sizes,aes(y=genes/1000,x=genome/1000000,colour=Genome))
+p <- ggplot(sizes,aes(y=gene_count/1000,x=genome_size/1000000,colour=Pseudomonas))
 p <- p + geom_point(size=10)
+p <- p + geom_text(aes(x = 6.25, y = 5.6, vjust = 1.5, hjust = -0.5, label = "R124"), colour = "black")
 p <- p + scale_y_continuous("Gene Count (000s)")
 p <- p + scale_x_continuous("Size (MBp)")
 p <- p + theme_bw()
