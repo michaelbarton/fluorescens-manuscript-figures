4b3ffd2d2cf5c6f19ff849e4ae686a5d87b47422
Michael Barton <mail@michaelbarton.me.uk>
2012-03-06 14:53:43 -0500

    Add figure keys to image

--- plot/genome_dot_plot.R
+++ plot/genome_dot_plot.R
@@ -27,16 +27,20 @@ data$reference <- as.factor(find.replace(data$reference,
   c("Pf0-1","Pf-5","SBW-25")
   ))
 
-base <- ggplot(data,aes(color=reference)) + theme_bw()
+base <- ggplot(data) + theme_bw()
 
-p1 <- base + geom_segment(size = 6,aes(y = V1/1000000, yend = V2/1000000,
-  x = V3/1000000, xend = V4/1000000))
+p1 <- base + geom_segment(aes(y = V1/1000000, yend = V2/1000000,
+  x = V3/1000000, xend = V4/1000000,color=reference),size = 6)
 p1 <- p1 + scale_y_continuous("P. fluorescens Reference Genome Position (MBp)")
 p1 <- p1 + xlab(NULL)
+p1 <- p1 + geom_text(aes(x,y,label = text),
+                     data.frame(x=0.25, y=7, text="A"))
 
-p2 <- base + geom_density(size = 1, adjust = 1/3, aes(x = (V4 + V3)/2000000))
+p2 <- base + geom_density(size = 1, adjust = 1/3, aes(x = (V4 + V3)/2000000,color=reference))
 p2 <- p2 + scale_x_continuous("P. fluorescens R124 Genome Position (MBp)")
 p2 <- p2 + scale_y_continuous("Sequence Alignment Density",breaks = c(0))
+p2 <- p2 + geom_text(aes(x,y,label = text),
+                     data.frame(x=0.25, y=0.25, text="B"))
 
 legend <- p2 + opts(keep = "legend_box")
 p1 <- p1 + opts(legend.position = "none")
